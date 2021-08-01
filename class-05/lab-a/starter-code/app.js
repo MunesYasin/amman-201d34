ba96b365bcbf5addb0d64baa1c260909566b2865
Mu’nes Yasin <86602263+MunesYasin@users.noreply.github.com>
2021-08-01 14:30:23 +0300

    Update app.js

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -130,16 +130,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
- let s = multiply(multArr[0], multArr[1]);
-  let m = s[0];
-  let n = multiply(m, multArr[2]);
-  let productsOfelements = n[0];
-return [ productsOfelements,`The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${productsOfelements}`];
+
 }
 
 
 // Here is the test for multiplyArray(); uncomment it to run it
-testmultiplyArray (testArray);
+//testmultiplyArray (testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
