11d390f56f3283f2b0356d7d5a40c4c60816b372
Mu’nes Yasin <86602263+MunesYasin@users.noreply.github.com>
2021-08-03 11:20:39 +0300

    Update app.js

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -31,7 +31,7 @@ function multiply(a, b) {
     let total = a * b;
   return [total, `The product of ${a} and ${b} is ${total}.`];
 }
-}
+
 
 // Here is the test for multiply(); uncomment it to run it
  testMultiply(5,9);
